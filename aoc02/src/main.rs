20f67929415c2d85751eb8211a6cd2e38ace9464
Christian Fosli <christian.fosli@webstep.no>
2024-12-02 22:48:43 +0100

    Day 2 part 1

---
+++ aoc02/src/main.rs
@@ -0,0 +1,86 @@
+use std::{error::Error, fs, str::FromStr};
+
+#[derive(Debug, Clone)]
+struct Reports {
+    nums: Vec<Vec<u8>>,
+}
+
+impl FromStr for Reports {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let nums = s
+            .lines()
+            .map(|l| {
+                l.split(" ")
+                    .map(|n| n.parse::<u8>().unwrap())
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+        Ok(Reports { nums })
+    }
+}
+
+fn part_1_count_safe(reports: &Reports) -> usize {
+    reports
+        .nums
+        .iter()
+        .filter_map(|levels| {
+            if (levels
+                .windows(2)
+                .map(|x| TryInto::<&[u8; 2]>::try_into(x).unwrap())
+                .all(|&[x, y]| y > x)
+                || levels
+                    .windows(2)
+                    .map(|x| TryInto::<&[u8; 2]>::try_into(x).unwrap())
+                    .all(|&[x, y]| x > y))
+                && (levels
+                    .windows(2)
+                    .map(|x| TryInto::<&[u8; 2]>::try_into(x).unwrap())
+                    .all(|&[x, y]| {
+                        let diff = (y as i8 - x as i8).abs();
+                        diff == 1 || diff == 2 || diff == 3
+                    }))
+            {
+                Some(levels)
+            } else {
+                None
+            }
+        })
+        .count()
+}
+
+fn part_2_count_dampened_safe(reports: &Reports) -> usize {
+    todo!("Not yet impl");
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("input.txt")?;
+    let reports = input.parse()?;
+    println!("Part 1: {}", part_1_count_safe(&reports));
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &'static str = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+
+    #[test]
+    fn it_passes_testcase_1() {
+        let reports = TEST_INPUT.parse::<Reports>().unwrap();
+        assert_eq!(2, part_1_count_safe(&reports));
+    }
+
+    #[test]
+    fn it_passes_testcase_2() {
+        let reports = TEST_INPUT.parse::<Reports>().unwrap();
+        assert_eq!(4, part_2_count_dampened_safe(&reports));
+    }
+}
