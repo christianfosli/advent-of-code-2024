c301d660c249dccb0be3b44b8daa5b32638b40e4
Christian Fosli <christian.fosli@webstep.no>
2024-12-14 20:35:49 +0100

    Day 13 part 2 wip

--- aoc13/src/main.rs
+++ aoc13/src/main.rs
@@ -1,12 +1,12 @@
-use std::{error::Error, fs, str::FromStr};
+use std::{borrow::Borrow, error::Error, fs, str::FromStr};
 
 use good_lp::{constraint, default_solver, variables, Solution, SolverModel};
 use regex::Regex;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 struct Point {
-    x: u32,
-    y: u32,
+    x: f64,
+    y: f64,
 }
 
 struct ClawMachineProblem {
@@ -26,8 +26,8 @@ impl FromStr for ClawMachineProblem {
             .captures(str_a)
             .unwrap();
         let btn_a = Point {
-            x: re_caps_a[1].parse::<u32>()?,
-            y: re_caps_a[2].parse::<u32>()?,
+            x: re_caps_a[1].parse::<f64>()?,
+            y: re_caps_a[2].parse::<f64>()?,
         };
 
         let str_b = lines_iter.next().unwrap();
@@ -35,8 +35,8 @@ impl FromStr for ClawMachineProblem {
             .captures(str_b)
             .unwrap();
         let btn_b = Point {
-            x: re_caps_b[1].parse::<u32>()?,
-            y: re_caps_b[2].parse::<u32>()?,
+            x: re_caps_b[1].parse::<f64>()?,
+            y: re_caps_b[2].parse::<f64>()?,
         };
 
         let str_prize = lines_iter.next().unwrap();
@@ -44,8 +44,8 @@ impl FromStr for ClawMachineProblem {
             .captures(str_prize)
             .unwrap();
         let prize = Point {
-            x: re_caps_p[1].parse::<u32>()?,
-            y: re_caps_p[2].parse::<u32>()?,
+            x: re_caps_p[1].parse::<f64>()?,
+            y: re_caps_p[2].parse::<f64>()?,
         };
 
         Ok(ClawMachineProblem {
@@ -57,10 +57,35 @@ impl FromStr for ClawMachineProblem {
 }
 
 impl ClawMachineProblem {
-    fn solve(&self) -> Option<f64> {
+    fn solve_1(&self) -> Option<f64> {
         variables! { vars:
             a (integer) <= 100;
             b (integer) <= 100;
+        }
+
+        let objective = a * 3 + b;
+
+        let result = vars
+            .minimise(&objective)
+            .using(default_solver)
+            .with(constraint!(
+                a * self.btn_a.x + b * self.btn_b.x == self.prize.x
+            ))
+            .with(constraint!(
+                a * self.btn_a.y + b * self.btn_b.y == self.prize.y
+            ))
+            .solve();
+
+        match result {
+            Ok(solution) => Some(solution.eval(&objective)),
+            Err(_) => None,
+        }
+    }
+
+    fn solve_2(self) -> Option<f64> {
+        variables! { vars:
+            a (integer);
+            b (integer);
         }
 
         let objective = a * 3 + b;
@@ -86,7 +111,22 @@ impl ClawMachineProblem {
 fn min_token_cost_p1(machines: &[ClawMachineProblem]) -> f64 {
     machines
         .into_iter()
-        .filter_map(ClawMachineProblem::solve)
+        .filter_map(ClawMachineProblem::solve_1)
+        .sum()
+}
+
+fn min_token_cost_p2(machines: &[ClawMachineProblem]) -> f64 {
+    machines
+        .into_iter()
+        .map(|m| ClawMachineProblem {
+            btn_a: m.btn_a,
+            btn_b: m.btn_b,
+            prize: Point {
+                x: m.prize.x + 10_000_000_000_000f64,
+                y: m.prize.y + 10_000_000_000_000f64,
+            },
+        })
+        .filter_map(ClawMachineProblem::solve_2)
         .sum()
 }
 
@@ -98,6 +138,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         .collect::<Result<Vec<_>, _>>()?;
 
     println!("Part 1: {}", min_token_cost_p1(&machines));
+    println!("Part 2: {}", min_token_cost_p2(&machines));
 
     Ok(())
 }
@@ -125,12 +166,15 @@ Prize: X=18641, Y=10279";
     #[test]
     fn test_solve() {
         let problem = ClawMachineProblem {
-            btn_a: Point { x: 94, y: 34 },
-            btn_b: Point { x: 22, y: 67 },
-            prize: Point { x: 8400, y: 5400 },
+            btn_a: Point { x: 94.0, y: 34.0 },
+            btn_b: Point { x: 22.0, y: 67.0 },
+            prize: Point {
+                x: 8400.0,
+                y: 5400.0,
+            },
         };
 
-        assert_eq!(Some(280f64), problem.solve());
+        assert_eq!(Some(280f64), problem.solve_1());
     }
 
     #[test]
